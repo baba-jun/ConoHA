58a9f4f19e38e501a54747534747f6d363a2c4cd
muhi111 <107345594+muhi111@users.noreply.github.com>
2024-08-06 18:41:00 +0900

    fix: fix vps component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import './App.css';
 import Header from "./components/Header";
-import MainContent from "./components/MainContent";
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Vps from './components/Vps';
 import Game from './components/Game';
 // import Sidebar from './components/Sidebar';
@@ -13,7 +12,7 @@ const App: React.FC = () => {
       <div className="content">
         {/* <Sidebar /> */}
         <Routes>
-        <Route path="/" element={<MainContent />} />
+        <Route path="/" element={<Navigate to="/vps"/>} />
         <Route path="/vps" element={<Vps />} />
         <Route path="/game" element={<Game />} />
         </Routes>
--- src/components/Vps.tsx
+++ src/components/Vps.tsx
@@ -1,46 +1,56 @@
-
 const Vps = () => {
-    return (
-      <main>
-        <section className="os-selection">
+  return (
+    <main className="vps-main">
+      <section className="service-section">
+        <h2>サービス</h2>
+        <div className="service-grid">
+          <div className="service-item active">VPS</div>
+          <div className="service-item">Windows Server</div>
+          <div className="service-item">GPUサーバー</div>
+          <div className="service-item">メールサーバー</div>
+          <div className="service-item">DBサーバー</div>
+        </div>
+      </section>
+      
+      <section className="os-selection">
         <h2>イメージタイプ</h2>
         <div className="os-grid">
-        <div className="radio-button">
-              <input type="radio" id="centos" name="os" value="CentOS" />
-              <label htmlFor="centos">CentOS</label>
-            </div>
-            <div className="radio-button">
-              <input type="radio" id="ubuntu" name="os" value="Ubuntu" />
-              <label htmlFor="ubuntu">Ubuntu</label>
-            </div>
-            <div className="radio-button">
-              <input type="radio" id="debian" name="os" value="Debian" />
-              <label htmlFor="debian">Debian</label>
-            </div>
-            <div className="radio-button">
-              <input type="radio" id="rocky" name="os" value="Rocky Linux" />
-              <label htmlFor="rocky">Rocky Linux</label>
-            </div>
-            <div className="radio-button">
-              <input type="radio" id="alma" name="os" value="AlmaLinux" />
-              <label htmlFor="alma">AlmaLinux</label>
-            </div>
-            <div className="radio-button">
-              <input type="radio" id="oracle" name="os" value="Oracle Linux" />
-              <label htmlFor="oracle">Oracle Linux</label>
-            </div>
-            <div className="radio-button">
-              <input type="radio" id="freebsd" name="os" value="FreeBSD" />
-              <label htmlFor="freebsd">FreeBSD</label>
-            </div>
-            <div className="radio-button">
-              <input type="radio" id="arch" name="os" value="Arch Linux" />
-              <label htmlFor="arch">Arch Linux</label>
-            </div>
+          <div className="radio-button">
+            <input type="radio" id="centos" name="os" value="CentOS" />
+            <label htmlFor="centos">CentOS</label>
           </div>
-        </section>
-        </main>
-    );
-  };
+          <div className="radio-button">
+            <input type="radio" id="ubuntu" name="os" value="Ubuntu" />
+            <label htmlFor="ubuntu">Ubuntu</label>
+          </div>
+          <div className="radio-button">
+            <input type="radio" id="debian" name="os" value="Debian" />
+            <label htmlFor="debian">Debian</label>
+          </div>
+          <div className="radio-button">
+            <input type="radio" id="rocky" name="os" value="Rocky Linux" />
+            <label htmlFor="rocky">Rocky Linux</label>
+          </div>
+          <div className="radio-button">
+            <input type="radio" id="alma" name="os" value="AlmaLinux" />
+            <label htmlFor="alma">AlmaLinux</label>
+          </div>
+          <div className="radio-button">
+            <input type="radio" id="oracle" name="os" value="Oracle Linux" />
+            <label htmlFor="oracle">Oracle Linux</label>
+          </div>
+          <div className="radio-button">
+            <input type="radio" id="freebsd" name="os" value="FreeBSD" />
+            <label htmlFor="freebsd">FreeBSD</label>
+          </div>
+          <div className="radio-button">
+            <input type="radio" id="arch" name="os" value="Arch Linux" />
+            <label htmlFor="arch">Arch Linux</label>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+};
 
-  export default Vps;
+export default Vps;
