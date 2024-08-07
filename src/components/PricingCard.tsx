e49b5d5a9e71b5d6d1d170a92f61be1796f1133a
baba-jun <msk3059i@gmail.com>
2024-08-07 09:59:58 +0900

    右端画面の描画内容切り替えボタンの実装完了

--- src/components/PricingCard.tsx
+++ src/components/PricingCard.tsx
@@ -3,34 +3,50 @@ import '../App.css';
 
 
 const PricingCard = () => {
+  const [isFareButton, setIsFareBUtton] = useState(false);
+  const [isChatButton, setIsChatbutton] = useState(false);
+
+  const handleChatButton = () => {
+    setIsChatbutton(true);
+  }
+
+  const handleFareButton = () => {
+    setIsFareBUtton(true);
+  }
 
 
   return (
-    <div className="pricing-card-wrapper">
-    <div className="pricing-card">
-      <div className="card-content">
-        <div className="service-details">
-          <p>サービス <span>VPS</span></p>
-          <p>CPU <span>6Core</span></p>
-          <p>メモリ <span>8GB</span></p>
-          <p>SSD <span>100GB</span></p>
-          <p>リージョン <span>東京</span></p>
+    <div>
+      {isFareButton && <div className="fare-button">料金比較</div>}
+      {isChatButton && <div className="chat-button">チャット画面</div>}
+      {!isFareButton && !isChatButton && (
+        <div className="pricing-card-wrapper">
+          <div className="pricing-card">
+            <div className="card-content">
+              <div className="service-details">
+                <p>サービス <span>VPS</span></p>
+                <p>CPU <span>6Core</span></p>
+                <p>メモリ <span>8GB</span></p>
+                <p>SSD <span>100GB</span></p>
+                <p>リージョン <span>東京</span></p>
+              </div>
+              <div className="billing-details">
+                <p>サーバー料金 <span>14.6 円/時間</span></p>
+                <p>VPS割引きっぷ <span>利用しない</span></p>
+              </div>
+              <div className="total">
+                <p>合計 <span>月額最大</span></p>
+                <p className="price">8,082 円/月</p>
+              </div>
+              <button className="add-button">追加</button>
+            </div>
+          </div>
+          <div className="button-container">
+            <button className="circle-button" onClick={handleFareButton}>料金比較</button>
+            <button className="circle-button" onClick={handleChatButton}>何かお困りですか？</button>
+          </div>
         </div>
-        <div className="billing-details">
-          <p>サーバー料金 <span>14.6 円/時間</span></p>
-          <p>VPS割引きっぷ <span>利用しない</span></p>
-        </div>
-        <div className="total">
-          <p>合計 <span>月額最大</span></p>
-          <p className="price">8,082 円/月</p>
-        </div>
-        <button className="add-button">追加</button>
-      </div>
-      </div>
-      <div className="button-container">
-        <button className="circle-button">料金比較</button>
-        <button className="circle-button">何かお困りですか？</button>
-    </div>
+      )}
     </div>
   );
 };
