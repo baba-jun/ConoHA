2d500a29771abca04655f25e25cf28f13c38462a
JustHiro55 <hagi.hiro9829@gmail.com>
2024-08-08 14:55:42 +0900

    fix: change create server request method

--- backend/main.go
+++ backend/main.go
@@ -37,7 +37,7 @@ func main() {
 		r.Use(corsHandler)
 		r.Post("/token/create", handlers.CreateTokenHandler)
 		r.Get("/price", handlers.GetPriceHandler)
-		r.Get("/server/create", handlers.CreateServerHandler)
+		r.Post("/server/create", handlers.CreateServerHandler)
 	})
 
 	port := os.Getenv("PORT")
