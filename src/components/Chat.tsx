7a5fa7d88d633eb1f1f79949b8d9ea1babf8703b
muhi111 <107345594+muhi111@users.noreply.github.com>
2024-08-09 10:49:26 +0900

    fix: fix with linter

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import "../Chat.css";
 import { API_URL } from "../main";
 
